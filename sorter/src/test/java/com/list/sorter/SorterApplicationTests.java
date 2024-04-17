a37d6fbe437e90985d39b5ecb87b3c263126937c
Jay <138824448+jaythemself@users.noreply.github.com>
2024-04-16 23:19:02 -0500

    initial commit

---
+++ sorter/src/test/java/com/list/sorter/SorterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.list.sorter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SorterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
